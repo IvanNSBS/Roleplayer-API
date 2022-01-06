cbcdf9f454f66f006cbc0f34efffafc26ee6a630
IvanNSBS <insbs@cin.ufpe.br>
2022-01-06 20:23:34 -0300

    Moving POCO to Behaviour Subfolder & Creating AbilitiesUser component

---
+++ Runtime/RPG/AbilitiesSystem/Components/AbilitiesUser.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace INUlib.RPG.AbilitiesSystem
+{
+    class AbilitiesUser : MonoBehaviour
+    {
+        #region Inspector Fields
+        #endregion
+
+        #region Fields
+        private AbilitiesController _controller;
+        #endregion
+
+
+        #region MonoBehaviour Methods
+        #endregion
+
+
+        #region Methods
+        #endregion
+    }
+}
