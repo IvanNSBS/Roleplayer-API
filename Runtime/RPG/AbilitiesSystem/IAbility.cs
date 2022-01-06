7eb5bafd87351781e21b12f42951f4ef9039e13c
IvanNSBS <insbs@cin.ufpe.br>
2022-01-06 17:13:59 -0300

    Removing PrepareCast Function that won't be used

--- Runtime/RPG/AbilitiesSystem/IAbility.cs
+++ Runtime/RPG/AbilitiesSystem/IAbility.cs
@@ -2,13 +2,6 @@ namespace INUlib.RPG.AbilitiesSystem
 {
     public interface IAbility
     {
-        /// <summary>
-        /// Prepares to cast the ability.
-        /// This is used to, for example, select and show 
-        /// the area that the ability will cover once cast.
-        /// You can invoke Cast 
-        /// </summary>
-        void PrepareCast();
         void Cast();
         float CurrentCooldown {get; set;}
         float Cooldown {get; set;}
