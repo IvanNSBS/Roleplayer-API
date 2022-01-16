505823c191830cd731bc1248f719709a4e278a32
IvanNSBS <insbs@cin.ufpe.br>
2022-01-16 12:39:05 -0300

    Documenting BehaviourTreeAsset

--- Runtime/Gameplay/AI/BehaviourTrees/Scriptables/BehaviourTreeAsset.cs
+++ Runtime/Gameplay/AI/BehaviourTrees/Scriptables/BehaviourTreeAsset.cs
@@ -21,12 +21,22 @@ namespace INUlib.Gameplay.AI.BehaviourTrees
         #region Methods
         public BehaviourTree CreateTree() => new BehaviourTree(_root.CreateNode());
 
+        /// <summary>
+        /// Creates the Root Node View for the Behaviour Tree Editor
+        /// </summary>
         public void CreateRoot()
         {
             if(_root == null)
                 _root = CreateNode(typeof(RootNode), Vector2.zero) as RootNode;
         }
         
+
+        /// <summary>
+        /// Creates a node inside te Behaviour Tree Editor Graph View
+        /// </summary>
+        /// <param name="t">The type of the node to create</param>
+        /// <param name="pos">Where in the editor ot spawn it</param>
+        /// <returns>The SerializedBTNode</returns>
         public SerializedBTNode CreateNode(Type t, Vector2 pos)
         {
             SerializedBTNode serialized = CreateBTNode(t, pos);
@@ -40,6 +50,11 @@ namespace INUlib.Gameplay.AI.BehaviourTrees
             return serialized;
         }
 
+        /// <summary>
+        /// Removes a node from the Behaviour Tree Editor Graph View
+        /// </summary>
+        /// <param name="node">The Serialized node to remove</param>
+        /// <returns>True if it was removed. False otherwise</returns>
         public bool RemoveNode(SerializedBTNode node)
         {
             bool removed = _inspectorNodes.Remove(node);
@@ -55,6 +70,12 @@ namespace INUlib.Gameplay.AI.BehaviourTrees
             return removed;
         }
 
+        /// <summary>
+        /// Utility Function to create a SerializedBTNode
+        /// </summary>
+        /// <param name="t">The node type</param>
+        /// <param name="pos">Where to spawn it</param>
+        /// <returns>The created SerializedBTNode</returns>
         private static SerializedBTNode CreateBTNode(Type t, Vector2 pos)
         {
             SerializedBTNode node = ScriptableObject.CreateInstance(t) as SerializedBTNode;
