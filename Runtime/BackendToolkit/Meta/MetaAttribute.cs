4b58e2fde2d3d9bec61588afcf84479e8c0c5074
IvanNSBS <insbs@cin.ufpe.br>
2022-03-21 15:09:10 -0300

    Adding a GameMetaLoader class that auto loads field/properties marked with the MetaAttribute

---
+++ Runtime/BackendToolkit/Meta/MetaAttribute.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace INUlib.BackendToolkit.Meta
+{
+    [System.AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public class MetaAttribute : Attribute
+    {
+    }
+}
