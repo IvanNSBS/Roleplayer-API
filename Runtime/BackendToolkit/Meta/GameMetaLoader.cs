f07700b6ba6b3a98d9e80144ae38c8622dd453eb
IvanNSBS <insbs@cin.ufpe.br>
2022-03-21 17:33:35 -0300

    GameMetaLoader safely ignores fields and properties that are not MetaFiles

--- Runtime/BackendToolkit/Meta/GameMetaLoader.cs
+++ Runtime/BackendToolkit/Meta/GameMetaLoader.cs
@@ -31,6 +31,13 @@ namespace INUlib.BackendToolkit.Meta
 
             foreach(FieldInfo field in validFields)
             {
+                bool valid = typeof(IBaseMetaFile).IsAssignableFrom(field.FieldType);
+                if(!valid)
+                {
+                    if(debug)
+                        Debug.Log($"Field <{field.Name}> is not a MetaFile");
+                    continue;
+                }
                 if(debug)
                     Debug.Log($"Instantiating Field <{field.Name}> with type <{field.FieldType}>");
                 
@@ -41,6 +48,13 @@ namespace INUlib.BackendToolkit.Meta
 
             foreach(PropertyInfo prop in validProps)
             {
+                bool valid = typeof(IBaseMetaFile).IsAssignableFrom(prop.PropertyType);
+                if(!valid)
+                {
+                    if(debug)
+                        Debug.Log($"Property <{prop.Name}> is not a MetaFile");
+                    continue;
+                }
                 if(debug)
                     Debug.Log($"Instantiating Property <{prop.Name}> with type <{prop.PropertyType}>");
                 
