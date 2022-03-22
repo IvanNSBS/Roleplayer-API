14c4dd717c383c842852ba9730cf3b8c9976334c
IvanNSBS <insbs@cin.ufpe.br>
2022-03-21 22:08:52 -0300

    Fixing "Play" button position

--- Editor/Core/AudioDataInspector.cs
+++ Editor/Core/AudioDataInspector.cs
@@ -1,7 +1,8 @@
 using UnityEditor;
 using UnityEngine;
 using INUlib.BackendToolkit.Audio;
-using UnityEngine.UIElements;
+using System.Reflection;
+using System.Collections.Generic;
 
 namespace INUlib.UEditor.Core
 {
@@ -10,6 +11,7 @@ namespace INUlib.UEditor.Core
     {
         #region Fields
         private float _btnHeight = 40;
+        private float _padding = 2;
         #endregion
 
 
@@ -19,13 +21,15 @@ namespace INUlib.UEditor.Core
             EditorGUI.PropertyField(position, property, label, true);
             if(property.isExpanded)
             {
-                float height = EditorGUI.GetPropertyHeight(property, true);
-                float width = Screen.width*0.3f;
-                float center = Screen.width/2 - width/2;
-                Rect btnRect = new Rect(center, height + _btnHeight*1.5f, width, _btnHeight*0.75f);
+                float btnWidth = Screen.width*0.3f;
+                float y = position.y + position.height - _btnHeight + _padding*0.5f;
+                float x = Screen.width/2 - btnWidth/2;
+                Rect btnRect = new Rect(x, y, btnWidth, _btnHeight*0.75f);
+
                 if(GUI.Button(btnRect, "Play"))
                 {
-                    Debug.Log("udiosasaduhsai");
+                    // AudioData data = (GetValue(property) as List<AudioData>)[0];
+                    // Debug.Log("Audio Id: " + data.Id);
                 }
             }
         }
@@ -34,9 +38,58 @@ namespace INUlib.UEditor.Core
         {
             float height = EditorGUI.GetPropertyHeight(property, true); 
             if(property.isExpanded)
-                height += _btnHeight;
+                height += _btnHeight + _padding;
 
             return height;
+        }
+
+        public object GetValue(SerializedProperty property)
+        {
+            System.Type parentType = property.serializedObject.targetObject.GetType();
+            System.Reflection.FieldInfo fi = GetFieldViaPath(parentType, property.propertyPath);
+            Debug.Log("Field Info: " + fi.FieldType); 
+            
+            return fi.GetValue(property.serializedObject.targetObject);
+        }
+
+        public System.Reflection.FieldInfo GetFieldViaPath(System.Type type, string path)
+        {
+            var flags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance;
+            var parent = type;
+            var fi = parent.GetField(path, flags);
+            var paths = path.Split('.');
+    
+            for (int i = 0; i < paths.Length; i++)
+            {
+                fi = parent.GetField(paths[i], flags);
+    
+                // there are only two container field type that can be serialized:
+                // Array and List<T>
+                if (fi.FieldType.IsArray)
+                {
+                    parent = fi.FieldType.GetElementType();
+                    i += 2;
+                    continue;
+                }
+    
+                if (fi.FieldType.IsGenericType)
+                {
+                    parent = fi.FieldType.GetGenericArguments()[0];
+                    i += 2;
+                    continue;
+                }
+    
+                if (fi != null)
+                {
+                    parent = fi.FieldType;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+    
+            return fi;
         }
         #endregion
     }
