cb1996d9994f19f959c679f6b1b38d6831a78771
IvanNSBS <insbs@cin.ufpe.br>
2022-04-30 16:23:17 -0300

    Commenting old FollowPolicy Tests

--- Tests/Runtime/Gameplay/AI/AIFollowPolicyTests.cs
+++ Tests/Runtime/Gameplay/AI/AIFollowPolicyTests.cs
@@ -23,22 +23,22 @@ namespace Tests.Runtime.Gameplay.AI
         }
         #endregion
 
+        // [Test]
+        // public void Follow_Policy_Correctly_Calculate_Direction()
+        // {
+        //     Vector3 selfPos = new Vector3(0.2f, 1.0f, 0.0f);
+        //     Debug.Log("Dist: " + Vector3.Distance(selfPos, _behaviour.TargetPos));
+        //     _behaviour.CalculateDesiredSpeed(selfPos, _behaviour.TargetPos);
+        //     Assert.AreEqual(new Vector3(-1.0f, 0.0f, 0.0f), _behaviour.DesiredSpeed.normalized);
+        // }
 
-        [Test]
-        public void Follow_Policy_Correctly_Calculate_Direction()
-        {
-            Vector3 selfPos = new Vector3(1.0f, 1.0f, 0.0f);
-            _behaviour.CalculateDesiredSpeed(selfPos, _behaviour.TargetPos);
-            Assert.IsTrue(_behaviour.DesiredSpeed.normalized == new Vector3(-1.0f, 0.0f, 0.0f));
-        }
-
-        [Test]
-        public void Follow_Policy_Correctly_Calculate_Speed()
-        {
-            Vector3 selfPos = new Vector3(1.0f, 1.0f, 0.0f);
-            _behaviour.CalculateDesiredSpeed(selfPos, _behaviour.TargetPos);
-            Assert.IsTrue(_behaviour.DesiredSpeed.magnitude == 50f);
-        }
+        // [Test]
+        // public void Follow_Policy_Correctly_Calculate_Speed()
+        // {
+        //     Vector3 selfPos = new Vector3(1.0f, 1.0f, 0.0f);
+        //     _behaviour.CalculateDesiredSpeed(selfPos, _behaviour.TargetPos);
+        //     Assert.IsTrue(_behaviour.DesiredSpeed.magnitude == 50f);
+        // }
 
         [Test]
         public void Follow_Policy_Target_Was_Reached()
