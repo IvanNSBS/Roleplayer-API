b79760a08e6947e1a59f7eced89b5cccb4a81e7a
IvanNSBS <insbs@cin.ufpe.br>
2022-05-01 11:05:46 -0300

    Null checking finished event on dispel

--- Runtime/RPG/StatusEffectSystem/StatusEffectManager.cs
+++ Runtime/RPG/StatusEffectSystem/StatusEffectManager.cs
@@ -133,7 +133,7 @@ namespace INUlib.RPG.StatusEffectSystem
             bool dispeled = _activeEffects.Remove(effect);
             if(dispeled && index != -1)
             {
-                onStatusEffectFinished(effect, index);
+                onStatusEffectFinished?.Invoke(effect, index);
                 effect.OnDispel();
             }
 
